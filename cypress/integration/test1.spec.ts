274c098396ed31d353fd85ad1d504680a02b0f04
nick <nick@kisanhub.com>
2022-04-23 16:55:56 +0100

    Dummy UI with 2 tests, 1 of which fails

---
+++ cypress/integration/test1.spec.ts
@@ -0,0 +1,14 @@
+context('test1', () => {
+
+  beforeEach( ()=> {
+    cy.visit('/');
+  });
+
+  it('should have the correct title', () => {
+    cy.get('h1').should('contain.text', 'Dummy UI');
+  })
+
+  it('this will fail', () => {
+    cy.get('h2').should('contain.text', 'xxx');
+  });
+});
