30ac7e82cc6b0b2a9079f3e2a169e14f047ab6b9
Nick Brook <nickbrook72@gmail.com>
2023-01-03 16:57:24 +0000

    this will fail

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,9 +7,7 @@ export default defineConfig({
   taskTimeout: 2000,
   chromeWebSecurity: false,
   retries: 1,
-
   e2e: {
-    baseUrl: 'http://localhost:4200',
     specPattern: 'cypress/e2e/**/*.spec.ts'
   }
 });
