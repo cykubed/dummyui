00927618aba5acb41a8f3d9a6e8e388438f9354a
Nick Brook <nickbrook72@gmail.com>
2023-02-03 17:22:20 +0000

    Lots of failures

---
+++ cypress/e2e/nonsense/test4.spec.ts
@@ -0,0 +1,11 @@
+context('test4', () => {
+
+  beforeEach( ()=> {
+    cy.visit('/');
+  });
+
+  it('should fail horribly again', () => {
+    cy.get('h2').should('contain.text', 'xxx');
+  });
+
+});
