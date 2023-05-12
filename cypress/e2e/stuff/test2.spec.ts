dadd15e1095627c647e75e71a6cd2427c83023d1
Nick Brook <nickbrook72@gmail.com>
2023-05-12 15:56:31 +0100

    prevert symlinks

--- cypress/e2e/stuff/test2.spec.ts
+++ cypress/e2e/stuff/test2.spec.ts
@@ -9,6 +9,23 @@ context('this is test2', () => {
   });
 
   it('non-skipped test that passes', () => {
+    cy.intercept({pathname: '/api/fish'}, {}).as('mock1')
     cy.get('h1').should('contain.text', 'Dummy UI');
   })
+  //
+  // it('sleep 10', ()=> {
+  //   cy.wait(1000);
+  // });
+  //
+  // it('sleep 65', ()=> {
+  //   cy.wait(65000);
+  // });
+
+  // it('another test that passes but with a delay', () => {
+  //   cy.intercept({pathname: '/api/fish'}, {}).as('mock2')
+  //   cy.get('h1').should('contain.text', 'Dummy UI');
+  //   cy.wait(10000);
+  //   cy.get('h1').should('contain.text', 'Dummy UI');
+  // })
+
 });
