b5fe41ce7742763ad83262078d1cf45c10c8a567
Nick Brook <nickbrook72@gmail.com>
2023-05-17 17:50:33 +0100

    dummy

--- cypress/e2e/stuff/test1.spec.ts
+++ cypress/e2e/stuff/test1.spec.ts
@@ -6,6 +6,7 @@ context('test1', () => {
     cy.visit('/');
   });
 
+
   it('should show next when button clicked', () => {
     cy.get('button').click();
     cy.get('#next-title').should('be.visible');
