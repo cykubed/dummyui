d86fc6d40c90859084a1227c2a109ea4e57d611d
Nick Brook <nickbrook72@gmail.com>
2023-05-18 12:32:57 +0100

    fewer fails

--- cypress/e2e/stuff/test1.spec.ts
+++ cypress/e2e/stuff/test1.spec.ts
@@ -21,13 +21,13 @@ context('test1', () => {
     shouldFail();
   });
 
-  it('this will also fail', () => {
-    cy.get('h2').should('contain.text', 'xxx');
-  });
-
-  it('this will fail as well', () => {
-    cy.get('h2').should('contain.text', 'xxx');
-  });
+  // it('this will also fail', () => {
+  //   cy.get('h2').should('contain.text', 'xxx');
+  // });
+  //
+  // it('this will fail as well', () => {
+  //   cy.get('h2').should('contain.text', 'xxx');
+  // });
 
   it.skip('this will be skipped', () => {
     cy.get('h2').should('contain.text', 'xxx');
